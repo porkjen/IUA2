9d046b84d187739c9e537fab3cb9a1154f4f1016
porkjen <a0966606346@gmail.com>
2023-08-16 20:12:13 +0800

    course

--- src/main/java/com/example/demo/Crawler.java
+++ src/main/java/com/example/demo/Crawler.java
@@ -70,8 +70,8 @@ public class Crawler {
                 int width = element.getSize().getWidth();
                 int height = element.getSize().getHeight();
 
-                //BufferedImage subImage = image.getSubimage(point.getX()+350, point.getY()+132, width + 6, height + 4);//朱
-                BufferedImage subImage = image.getSubimage(point.getX()+205, point.getY()+69, width + 6, height + 4);//31
+                BufferedImage subImage = image.getSubimage(point.getX()+350, point.getY()+132, width + 6, height + 4);//朱
+                //BufferedImage subImage = image.getSubimage(point.getX()+205, point.getY()+69, width + 6, height + 4);//31
                 //BufferedImage subImage = image.getSubimage(point.getX()+120, point.getY()+55, width + 6, height + 4);
                 ImageIO.write(subImage, "png", screenshot);
                 File screenshotLocation = new File("test.png");
