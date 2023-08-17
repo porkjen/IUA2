7b137b20f0f6853debb98fb3b513a48558aa3bb8
Victoria.White <victoria91718@gmail.com>
2023-08-17 16:33:05 +0800

    chatroom Updated

--- my-app/src/ChatRoomList.js
+++ my-app/src/ChatRoomList.js
@@ -1,39 +1,38 @@
 import './ChatRoomList.css';
-import React from 'react';
-import { useEffect, useState, useRef,  } from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 
-const ChatRoomList = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredChatRooms, setFilteredChatRooms] = useState([]);
-
-  const chatRooms = [
-    { id: 'R1', name: 'room1' },
-    { id: 'R2', name: 'room2' },
+function ChatRoomList() {
+  const [userName, setUserName] = useState('');
+  const [connected, setConnected] = useState(false);
+  const [chatRooms, setChatRooms] = useState([
+    { id: 'R1', name: '缺體育' },
+    { id: 'R2', name: '山海觀山缺一' },
     { id: 'R3', name: '徵室友' },
-    { id: 'R4', name: 'room4' },
-];
-  var stompClient = null;
-  var socket = new SockJS('/chatroom');
-            stompClient = Stomp.over(socket);
-            console.log("Connected!!");
-            // userName = document.getElementById('from').value;
-            // stompClient.connect({user:userName}, function(frame) {
-                // setConnected(true);
-                
-
-                // stompClient.subscribe('/topic/'+ chatRoom.name, function(messageOutput) {
-                //     showMessageOutput(JSON.parse(messageOutput.body));
-                // });
-
-                // // 私人
-                // stompClient.subscribe('/user/subscribe', function(messageOutput) {
-                //     showMessageOutput(JSON.parse(messageOutput.body));
-                // });
-            // });
-  
+    { id: 'R4', name: '403、404換課' }
+  ]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredChatRooms, setFilteredChatRooms] = useState(chatRooms);
+/*
+  const connectWebSocket = () => {
+    const socket = new SockJS('/chatroom');
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({ user: userName }, (frame) => {
+      setConnected(true);
+      console.log('Connected: ' + frame);
+      // 訂閱相關主題
+      stompClient.subscribe('/topic/messages', (messageOutput) => {
+        this.showMessageOutput(JSON.parse(messageOutput.body));// 處理訊息
+      });
+      // 私人
+      stompClient.subscribe('/user/subscribe', (messageOutput) => {
+        this.showMessageOutput(JSON.parse(messageOutput.body));// 處理訊息
+      });
+    });
+  };
+*/
   const handleSearch = (event) => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
@@ -45,12 +44,12 @@ const ChatRoomList = () => {
   };
 
   const linkStyle = {
-    color: 'black', 
-    textDecoration: 'none' // 去除底線
+    color: 'black',
+    textDecoration: 'none', // Remove underline
   };
 
   return (
-    <div className='ChatRoomList'>
+    <div className="ChatRoomList">
       <input
         type="text"
         placeholder="Search chat rooms"
@@ -58,24 +57,34 @@ const ChatRoomList = () => {
         onChange={handleSearch}
       />
       <ul>
-      {searchTerm
+        {searchTerm
           ? filteredChatRooms.map((chatRoom) => (
-            <Link to={`/chatroom/${chatRoom.id}`} style={linkStyle}  key={chatRoom.id}>
-              <li>
-                <span className="link-text">{chatRoom.name}</span>
-              </li>
-            </Link>
-          ))
+              <Link to={`/chatroom/${chatRoom.id}`} style={linkStyle}
+                    key={chatRoom.id} onClick={() => {
+                            localStorage.setItem('nowRoom', chatRoom.id);
+                           // connectWebSocket();
+                            }}>
+                <li>
+                  <span className="link-text">{chatRoom.name}</span>
+                </li>
+              </Link>
+            ))
           : chatRooms.map((chatRoom) => (
-            <Link to={`/chatroom/${chatRoom.id}`} style={linkStyle}  key={chatRoom.id}>
-              <li>
-                <span className="link-text">{chatRoom.name}</span>
-              </li>
-            </Link>
-          ))}
+              <Link to={`/chatroom/${chatRoom.id}`} style={linkStyle}
+                                  key={chatRoom.id} onClick={() => {
+                                          localStorage.setItem('nowRoom', chatRoom.id);
+                                          localStorage.setItem('nowRoomName', chatRoom.name)
+                                          localStorage.setItem('userName', 'White')
+                                          //connectWebSocket();
+                                          }}>
+                              <li>
+                  <span className="link-text">{chatRoom.name}</span>
+                </li>
+              </Link>
+            ))}
       </ul>
     </div>
   );
-};
+}
 
 export default ChatRoomList;
