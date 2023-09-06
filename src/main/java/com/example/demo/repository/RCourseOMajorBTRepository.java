4a4cc281d71a8a5d899eac2e23ec59f2eee197c4
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2023-09-06 17:41:45 +0800

    Update RCourseOMajorBTRepository.java

--- src/main/java/com/example/demo/repository/RCourseOMajorBTRepository.java
+++ src/main/java/com/example/demo/repository/RCourseOMajorBTRepository.java
@@ -12,4 +12,8 @@ public interface RCourseOMajorBTRepository extends MongoRepository<RequiredCours
     @Query(value = "{'c_category': ?0,'c_grade': {$regex: ?1}}")
     List<RequiredCourseEntityOuterMajorBT> findByCategoryAndGrade(String c_category, String c_grade);
 
+    //在資料庫item內查找c_number&c_grade(regex is include)
+    @Query(value = "{'c_number': ?0,'c_grade': {$regex: ?1}}")
+    List<RequiredCourseEntityOuterMajorBT> findByNumberAndGrade(String c_number, String c_grade);
+
 }
