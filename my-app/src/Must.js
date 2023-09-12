1841d780a31bb36f3b3437a8d29dedccd3be9afb
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2023-09-12 14:04:18 +0800

    Update Must.js

--- my-app/src/Must.js
+++ my-app/src/Must.js
@@ -1,14 +1,63 @@
 import './Must.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import back from './img/back.png';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { Mustinfo } from './components/Style';
+import { Mustinfo, DetailBtn } from './components/Style';
+import Modal from 'react-modal';
 
 const Must = () => {
   function MustComponent() {
+    const [modalIsOpen, setModalIsOpen] = useState(false); // 控制彈出視窗的狀態
+    const [selectedCourse, setSelectedCourse] = useState(null); // 存儲選中的課程
+    const [data, setData] = useState([]);
     const location = useLocation();
     const RCResult = location.state?.RCResult || []; // 獲取查詢結果
+
+    const openModal = (courseNumber, courseMajor, cousrGrade) => {
+      const queryParams = new URLSearchParams({
+        number: courseNumber,
+        major: courseMajor,
+        grade: cousrGrade
+      });
+
+      const url = '/course_search_detail?' + queryParams.toString();
+
+      const formData = {
+        "number": courseNumber,
+        "major": courseMajor,
+        "grade": cousrGrade
+      };
+
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(queryParams.toString());
+          console.log(data);
+          setSelectedCourse(data);
+          setData(data);
+          setModalIsOpen(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+
+    const closeModal = () => {
+      setSelectedCourse(null);
+      setModalIsOpen(false);
+    };
+
+    const linkStyle = {
+      color: 'black',
+      textDecoration: 'none',
+    };
 
     return (
       <div className="Must">
@@ -24,14 +73,43 @@ const Must = () => {
           <div className='mustLable'>
             <div className="scrollableContainer">
               {RCResult.map((item) => (
-                <Mustinfo key={item.id}>
-                  {item.cname}<br />{item.cnumber}&emsp;{item.cgrade}&emsp;{item.cteacher}&emsp;{item.ccredit}學分
-                </Mustinfo>
+                  <Mustinfo key={item.id} onClick={() => openModal(item.cnumber, item.cmajor, item.cgrade)}>
+                    {item.cname}<DetailBtn>詳細資訊</DetailBtn>{item.cnumber}&emsp;{item.cgrade}&emsp;{item.cteacher}&emsp;{item.ccredit}學分
+
+                  </Mustinfo>
               ))}
             </div>
           </div>
         </div>
+        <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="課程詳細資訊"
+      >
+        {data.map((item) => (
+          <div>
+            <h2>{item.cname}</h2>
+            <p>課程代號：{item.cnumber}</p>
+             <p>選課類別：{item.ccategory}</p>
+             <p>課程學分：{item.ccredit}</p>
+             <p>授課教師：{item.cteacher}</p>
+             <p>開課年班：{item.cgrade}</p>
+             <p>上課時間：{item.ctime}</p>
+             <p>上課地點：{item.clocation}</p>
+             <p>人數上限：{item.cpeople}</p>
+             <p>教學目標：{item.cobjective}</p>
+             <p>先修科目：{item.cprecourse}</p>
+             <p>教材內容：{item.coutline}</p>
+             <p>教學方式：{item.ctmethod}</p>
+             <p>參考書目：{item.creference}</p>
+             <p>教學進度：{item.csyllabus}</p>
+             <p>評分方式:{item.cevaluation}</p>
+          </div>
+        ))}
+        <button onClick={closeModal}>關閉</button>
+      </Modal>
       </div>
+
     );
   }
 
