493f1adfdc766eb04fcb3488cacb0d6a49435703
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2023-09-13 18:51:09 +0800

    Update ChatSystemController.java

--- src/main/java/com/example/demo/controller/ChatSystemController.java
+++ src/main/java/com/example/demo/controller/ChatSystemController.java
@@ -3,11 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.controller.chat.Output;
 import com.example.demo.controller.chat.Message;
 import com.example.demo.Ws.MessageTemplate;
+import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.Date;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/system", method = RequestMethod.POST)
@@ -16,17 +17,33 @@ public class ChatSystemController {
     @Autowired
     private MessageTemplate template;
 
+    @Autowired
+    private TodoService todoService;
+
     @RequestMapping("/broadcast")
     public Output broadcast(@RequestBody Message message) {
         Output outputMessage = new Output(new Date().toString(), message);
         template.broadcast(outputMessage);
+        //System.out.println("broadcast");
         return outputMessage;
+    }
+
+    @GetMapping("/namecast")
+    @ResponseBody
+    public String namecast() {
+        // 訪問TodoController的studentID
+        String account = todoService.getAccount();
+        System.out.println(account);
+
+        // 返回純文字
+        return account;
     }
 
     @RequestMapping("/send/{user}")
     public Output broadcast(@PathVariable("user") String user, @RequestBody Message message) {
         Output output = new Output(new Date().toString(), message);
         template.sendMsgToUser(user, output);
+        //System.out.println("send_user");
         return output;
 
     }
