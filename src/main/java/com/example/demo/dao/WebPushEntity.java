8a3ad3e913dc0165bedb556da6500352217ffa3d
shanyiii <94973828+shanyiii@users.noreply.github.com>
2023-10-16 23:47:24 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/dao/WebPushEntity.java
@@ -0,0 +1,32 @@
+package com.example.demo.dao;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+
+@Document(collection = "WebPushCollection")
+public class WebPushEntity {
+    private String id;
+    private String studentID;
+    private String token;
+    private ArrayList<PushNotificationRequest> noticications;
+
+    public WebPushEntity(){}
+
+    public void setStudentID(String id){
+        this.studentID = id;
+    }
+    public String getStudentID(){
+        return studentID;
+    }
+    public void setToken(String token){
+        this.token = token;
+    }
+    public String getToken(){
+        return token;
+    }
+    public void setNotifications(ArrayList<PushNotificationRequest> noticications){
+        this.noticications = noticications;
+    }
+    public ArrayList<PushNotificationRequest> getNotifications(){
+        return noticications;
+    }
+}
